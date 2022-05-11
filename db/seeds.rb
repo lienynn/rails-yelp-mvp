da2e4d8fc784683df0f17a082d84be4eabb6c766
Lien Nguyen <lienhnguyen@outlook.com>
2022-05-11 16:46:43 -0400

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,9 +11,9 @@ puts 'Creating 5 fake restaurants...'
 5.times do
   restaurant = Restaurant.new(
     name:    Faker::Company.name,
-    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
-    phone_number: "#{Faker::PhoneNumber.phone_number}",
-    category: %w[chinese italian japanese french belgian].sample
+    address: " Address: #{Faker::Address.street_address}, #{Faker::Address.city}",
+    phone_number: "Phone Number: #{Faker::PhoneNumber.phone_number}",
+    category: 'Category: %w[Chinese Italian Japanese French Belgian].sample'
   )
   restaurant.save!
 end
