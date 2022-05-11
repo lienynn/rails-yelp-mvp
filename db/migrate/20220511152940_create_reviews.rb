d71c1b102154f42975314d149038f6fc6561ac73
Lien Nguyen <lienhnguyen@outlook.com>
2022-05-11 15:30:57 -0400

    update

--- db/migrate/20220511152940_create_reviews.rb
+++ db/migrate/20220511152940_create_reviews.rb
@@ -2,7 +2,7 @@ class CreateReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :reviews do |t|
       t.integer :rating
-      t.string :content
+      t.text :content
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_11_152940) do
+ActiveRecord::Schema.define(version: 2022_05_11_162844) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2022_05_11_152940) do
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "restaurant_id"
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
   end
 
 end
